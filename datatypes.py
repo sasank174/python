af243ae8e4333196b98031e2aaa24c3b3af1faa1
sasank174 <perumallasasank123@gmail.com>
2022-01-18 16:38:22 +0530

    commit

---
+++ datatypes.py
@@ -0,0 +1,82 @@
+x = "sa"
+print(type(x))
+print(x)
+print("==========")
+
+x = 1
+print(type(x))
+print(x)
+print("==========")
+
+x = 1.1
+print(type(x))
+print(x)
+print("==========")
+
+x = complex(1j)
+print(type(x)) 
+print(x.real)
+print(x.imag)
+
+print("==========================================")
+
+
+x = [1,1.2,"sasa"]
+print(type(x))
+print(x)
+print("==========")
+
+x = (1,1.2,"sasa")
+print(type(x))
+print(x)
+print("==========")
+
+x = range(10)
+print(type(x))
+print(x)
+
+print("==========================================")
+
+
+x = {1,1.2,"sasa"}
+print(type(x))
+print(x)
+print("==========")
+
+
+x = frozenset((1,1.2,"sasa"))
+print(type(x))
+print(x)
+
+print("==========================================")
+
+
+x = {"1":1,"1.2":1.2,"sasa":"sasnk"}
+print(type(x))
+print(x)
+
+print("==========================================")
+
+
+x = True
+print(type(x))
+print(x)
+
+
+print("==========================================")
+
+
+x = b"Hello"
+print(type(x))
+print(x)
+print("==========")
+
+x = bytearray(5)
+print(type(x))
+print(x)
+print("==========")
+
+x = memoryview(bytes(5))
+print(type(x))
+print(x)
+print("==========")
