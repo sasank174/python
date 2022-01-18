7d8a6fdebccb483c55578840bea151c7a0cd5402
sasank174 <perumallasasank123@gmail.com>
2022-01-18 18:18:45 +0530

    commit

---
+++ 7string_methods.py
@@ -0,0 +1,14 @@
+x = "hello i am sasank"
+
+print(x.capitalize())
+print(x.casefold())
+print(x.center(50,"*"))
+print(x.count("sasank"))
+print(x.endswith("sasank."))
+print(x.find("sasank"))
+print(x.index("sasank"))
+print(x.isalnum())
+print(x.isalpha())
+print(x.isdigit())
+print(x.islower())
+print(x.isnumeric())
