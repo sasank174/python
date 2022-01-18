a26f780ae8dd19b06ba0931f780294244c460776
sasank174 <perumallasasank123@gmail.com>
2022-01-18 16:52:03 +0530

    commit

---
+++ string.py
@@ -0,0 +1,47 @@
+x = "sasank"
+print(x)
+
+
+x = """
+ansdas
+ad
+ad
+as
+d
+asd
+a
+dasakbdkada
+"""
+print(x)
+
+
+
+x = "sasank"
+print(x[1])
+
+
+for i in x:print(i,end=" ")
+print()
+
+print("sa" in x)
+
+print(len(x))
+print(x[:2])
+print(x[-4:-1])
+
+
+print(x.upper())
+print(x.lower())
+print(x.capitalize())
+print()
+
+print(x.strip())
+print(x.replace("s","a"))
+print()
+
+print(x.split(" "))
+print("".join(x))
+print()
+
+
+print(x+x)
