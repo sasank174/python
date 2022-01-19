7cd6cc0e6ad8eb6adb2c84f578d112d4e38c1bce
sasank174 <perumallasasank123@gmail.com>
2022-01-20 00:10:05 +0530

    commit

--- 16functions.py
+++ 16functions.py
@@ -1,4 +1,25 @@
 def func():
     print("sasank")
-
 func()
+
+def func(s,a="hee"):
+    print(a,s)
+func("sasank","hai")
+
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+  print(type(kids))
+
+my_function("Emil", "Tobias", "Linus")
+
+def my_function(**kids):
+  print("The youngest child is " + kids["s"])
+  print(type(kids))
+
+my_function(s = "Emil", a = "Tobias", b ="Linus")
+
+
+def func(s):
+    return s*2
+print(func("sasank"))
