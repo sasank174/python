90dd6b790a50c684b7800daceac32f2ddc14d3c3
sasank174 <perumallasasank123@gmail.com>
2022-01-19 17:44:20 +0530

    commit

---
+++ 10tuple.py
@@ -0,0 +1,17 @@
+tup = (1,2,"asa",1.3,10)
+print(tup)
+print(type(tup))
+
+print(len(tup))
+
+
+t = ("sasank",)    # tuple with single variable
+print(t)
+print(type(t))
+
+print(tup[1])
+print("asa" in tup)
+
+
+tup = tup + t
+print(tup)
